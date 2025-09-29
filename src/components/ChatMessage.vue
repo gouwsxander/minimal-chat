eae56da460a2da5c5aee7d2d28fd5afb994359d5
Xander <gouws.xander@gmail.com>
2025-09-29 08:57:07 -0300

    Adds Markdown support for chat messages (using Showdown)

--- src/components/ChatMessage.vue
+++ src/components/ChatMessage.vue
@@ -1,25 +1,40 @@
 <template>
-  <div :class="{ 'user-message': isUser, 'bot-message': !isUser }">
-    {{ props.content }}
+  <div
+    id="container"
+    :class="{ 'user-message': isUser, 'bot-message': !isUser }"
+  >
+    <!-- {{ props.content }} -->
+    <div id="messageContent" v-html="covertToHtml(props.content)"></div>
   </div>
 </template>
 
 <script setup>
 import { defineProps } from "vue";
+import showdown from "showdown";
 
 const props = defineProps({
   isUser: Boolean,
   content: String,
 });
+
+var converter = new showdown.Converter();
+function covertToHtml(text) {
+  return converter.makeHtml(text);
+}
 </script>
 
 <style scoped>
-div {
-  padding: 1rem;
+#container {
+  padding: 0.1rem 1.8rem;
   margin: 1rem;
   color: black;
   font-size: 1rem;
   border-radius: 1rem;
+}
+
+#messageContent {
+  padding: 0rem;
+  margin: 0rem;
 }
 
 .user-message {
