260904873b7b721456d7dac04ff399f6f48a3447
Xander <gouws.xander@gmail.com>
2025-12-06 15:38:18 -0400

    chore: Fix styles

--- app/app.vue
+++ app/app.vue
@@ -1,31 +1,3 @@
 <template>
-  <ChatView id="chat" />
+    <ChatView id="chat" />
 </template>
-
-<script>
-import ChatView from "./components/ChatView.vue";
-
-export default {
-  name: "App",
-  components: {
-    ChatView,
-  },
-};
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  /*text-align: center;*/
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-#chat {
-  width: 100%;
-  max-width: 50rem;
-  margin: 0 auto;
-}
-</style>
